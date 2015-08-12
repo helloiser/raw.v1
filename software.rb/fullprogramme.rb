6960fe7ff53dcb8d2df836fd8f33067296c752ef
helloiser <w.ackroydwa@gmail.com>
2015-08-12 12:22:15 +1200

    Create fullprogramme.rb

---
+++ software.rb/fullprogramme.rb
@@ -0,0 +1,23 @@
+@RAW_main = ["?"]
+print "Welcome to raw.v2. Please type your first name >"
+@RAW_main[0] = gets.chomp
+print "Hello, " + @RAW_main[0] + ". Please add a descriptive word about you."
+@RAW_main[1] = gets.chomp
+print "We have identified you as " + @RAW_main[0] + " - " + @RAW_main[1] + ". Please make a password >"
+@RAW_main[2] = gets.chomp
+print "Please enter this password you have made >"
+def incorrect_pass
+print "Passcode incorrect, please re-enter >"
+@RAW_main[3] = gets.chomp
+if @RAW_main[3] === @RAW_main[2]
+@confirm = true
+else
+incorrect_pass
+end
+end
+@RAW_main[3] = gets.chomp
+if @RAW_main[3] === @RAW_main[2] || @confirm === true
+print "Passcode correct! "
+else
+incorrect_pass
+end
